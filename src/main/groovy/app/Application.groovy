32d99ad0b61f92cabd5ff2d6520422835ed3b652
Christoph Frick <rid@ofnir.net>
2014-10-26 13:27:19 +0100

    init

---
+++ src/main/groovy/app/Application.groovy
@@ -0,0 +1,10 @@
+import org.springframework.boot.*
+import org.springframework.boot.autoconfigure.*
+import org.springframework.stereotype.*
+
+@EnableAutoConfiguration
+class Application {
+	static void main(final String... args) {
+		SpringApplication.run(Application, args)
+	}
+}
