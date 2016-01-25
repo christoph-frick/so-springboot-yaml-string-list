0b5a9d20d812cea2daa0225de876e7a7015d3697
Christoph Frick <rid@ofnir.net>
2016-01-25 15:19:32 +0100

    update to recent springboot version; simplified main

--- src/main/groovy/app/Application.groovy
+++ src/main/groovy/app/Application.groovy
@@ -1,16 +1,12 @@
 package app
 
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.*
 import org.springframework.boot.autoconfigure.*
-import org.springframework.context.annotation.ComponentScan
-import org.springframework.stereotype.*
 
-@EnableAutoConfiguration
-@ComponentScan
+@SpringBootApplication
 class Application {
 	static void main(final String... args) {
 		def ctx = SpringApplication.run(Application, args)
-        println ctx.getBean(Config).filenames
+	        assert ctx.getBean(Config).filenames == ["test1", "test2"]
 	}
 }
